715cd95757382335445437ed5120d8b5245e6c8a
mrnekolate <julicamilo280@hotmail.com>
2024-11-25 14:03:44 -0500

    la prueba de mensaje de git es interesante tambien la consola de comandos :)

---
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bancolombia/config/PostgresqlConnectionProperties.java
@@ -0,0 +1,15 @@
+package co.com.bancolombia.config;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "adapters.postgres")
+public record PostgresqlConnectionProperties(
+        String host,
+        Integer port,
+        String dbname,
+        String schema,
+        String username,
+        String password) {
+
+}
